d8e4d90a790b195eee9c4f331c733571753777ec
ERE <ernesto_711@live.com.mx>
2022-03-16 23:39:16 -0600

    se comenzo la implementacion de las validaciones del form

--- index.js
+++ index.js
@@ -1,64 +1,85 @@
-/* const express    = require("express");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+const buttonSend = document.getElementById('btnSend');
+const inputs     = document.querySelectorAll('.form__input');
 
-const app = express();
+const regExpList = {
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.,
+    email:  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+    asunto: /^[a-zA-Z0-9][a-zA-Z0-9/s]/,
+    mensaje: /^[a-zA-Z0-9][a-zA-Z0-9/s]/,
+};
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+const fields = {
+    nombre: false,
+    email: false,
+    asunto: false,
+    mensaje: false
+};
 
+const validarTextovacio = (input, field)=>{
+    if(input.value == ''){
+        fields[field] = false;
+        console.log('incorrecta ' + input.value);
+        
+    }else{
+        console.log('correcta ' + input.value);
+        fields[field] = true;
+    }
+};
 
-app.get("/", (req,res)=>{
-    res.sendFile(__dirname + "/index.html");
-});
+const validarFormulario = (e) => {
 
-app.post("/",(req,res)=>{
+    switch (e.target.name){
+        case "nombre":
+            validarField(regExpList.nombre, e.target,'nombre');
+            break;
+        case "email":
+            validarField(regExpList.email, e.target, 'email');
+            break;
+            case 'asunto':
+            validarField(regExpList.asunto, e.target, 'asunto');
+            break;
+        case 'mensaje':
+            validarField(regExpList.mensaje, e.target, 'mensaje');
+            break;
+    }   
+};
 
-    let nombre = req.body.nombre;
-    let email = req.body.email;
-    let asunto = req.body.asunto;
-    let mensaje = req.body.mensaje;
-    let contentHtml = `
-        <h1>User information</h1>
-            <ul>
-            <li>nombre: ${nombre}</li>
-            <li>asunto: ${asunto}</li>
-            <li>email: ${email}</li>
-            <li>mensaje: ${mensaje}</li>
-            </ul>
-    `;
+const validarField = (expresion, input, field) => {
 
+    if(expresion.test(input.value)){
+        console.log('correcta ' + input.value);
+        
+        fields[field] = true;
 
-    var transporter = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com",
-        secureConnection: false,
-        port: 587,
-        auth: {
-            user: "ernesto_711@live.com.mx",
-            pass: "busterswordpok02"
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-
-    var mailOptions = {
-        from: "ernesto_711@live.com.mx",
-        to: "ernesto_711@live.com.mx",
-        subject: "Contacto desde Portafolio Web",
-        html: contentHtml
+    }else{
+        console.log('incorrecta ' + input.value);
+        fields[field] = false;
     }
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            res.status(500).send(error.message);
-        } else {
-            console.log("Email Enviado");
-            res.status(200).send("<h1>El email fue enviado</h1>");
-        }
-    });
+};
+
+
+inputs.forEach(input => {
+   input.addEventListener('keyup', validarFormulario);
+   input.addEventListener('blur', validarFormulario);
+   
 });
 
-app.listen(process.env.PORT, ()=>{
-    console.log("Server is on port 3000");
-}); */
+
+buttonSend.addEventListener('click',(e) => {
+
+    if(fields.nombre && fields.email && fields.asunto && fields.mensaje){
+
+        console.log('tu mensaje se envio');
+        
+    }else{
+        
+        console.log(fields.nombre);
+        console.log(fields.email);
+        console.log(fields.asunto);
+        console.log(fields.mensaje);
+        console.log("te falta un parametro");
+    }
+
+
+});
